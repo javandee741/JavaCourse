3ee8b5d597f08628fa2ce5edc9815ed4d93fd05f
javandee741 <javandee741@gmail.com>
2026-04-01 15:32:20 +0300

    Lesson 7 commit

---
+++ src/Lesson7/Final.java
@@ -0,0 +1,9 @@
+package Lesson7;
+
+public class Final {
+    static <name> void main() {
+        final String name = "Evgeny";
+        //we can't change variable
+        System.out.println("My name is " + name);
+    }
+}
