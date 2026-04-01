c818d61179ad24f0a8484c47ab508a04f6ccaa6a
javandee741 <javandee741@gmail.com>
2026-04-01 15:23:33 +0300

    Lesson 6 commit

---
+++ src/Lesson6/Casting.java
@@ -0,0 +1,12 @@
+package Lesson6;
+
+public class Casting {
+    static void main() {
+        int intCount = 12;
+        byte byteNumber = (byte) intCount;
+
+        byte byteCount = 22;
+        int intNumber = byteCount;
+
+    }
+}
