e4059236b95a8d8a5c7c6419682ede6e1b72b77b
javandee741 <javandee741@gmail.com>
2026-04-02 10:16:35 +0300

    Lesson 12 commit

---
+++ src/Lesson12/KotlinString.kt
@@ -0,0 +1,10 @@
+package Lesson12
+
+fun main() {
+    val myName: String = "Evgeny Markin"
+    println("Hello, $myName")
+
+    var a: Int=4
+    var b:Int=3
+    println("The sum a + b = ${a + b}")
+}
