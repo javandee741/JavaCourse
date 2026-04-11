533eebc459af0781ad019f51b02b166804f1401d
javandee741 <javandee741@gmail.com>
2026-04-11 10:23:53 +0300

    Commit Lesson 47 and 48

--- src/Lesson47and48/TryCatchDemo.java
+++ src/Lesson47and48/TryCatchDemo.java
@@ -1,15 +1,19 @@
-package Lesson47;
+package Lesson47and48;
 
 public class TryCatchDemo {
     static void main() {
-        int zero = 0;
+        int zero = 1;
         int count = 12;
+        String string = null;
 
         try {
             int result = count/zero;
             System.out.println(result);
+            System.out.println(string.length());
         } catch (ArithmeticException exception) {
             System.out.println("Division by zero. You're not Chuсk Norris!");
+        } catch (NullPointerException exception){
+            System.out.println("String has length null");
         } finally {
             System.out.println("The end try-catch block.");
         }
